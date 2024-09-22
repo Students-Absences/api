7dd6c8a360632342693dd94c5be2a87d4b1144fd
Stratis Dermanoutsos <stratis450@gmail.com>
2024-09-22 11:33:37 +0300

    Auto migrate project

--- api/Program.cs
+++ api/Program.cs
@@ -24,6 +24,12 @@ builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
